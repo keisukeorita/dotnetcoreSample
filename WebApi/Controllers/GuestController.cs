a21f5be6e917ad001397d593985915e06fa8a535
Keisuke Orita <orita.keisuke26@gmail.com>
2020-12-23 00:03:42 +0900

    Add put method to webapi Call Put Method of Webapi by blazor

--- WebApi/Controllers/GuestController.cs
+++ WebApi/Controllers/GuestController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using WebApi.Models;
 
 namespace WebApi.Controllers
@@ -34,6 +35,38 @@ namespace WebApi.Controllers
 
             return Guest;
         }
+
+        // PUT: api/Guest/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGuest(long id, Guest Guest)
+        {
+            if (id != Guest.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(Guest).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GuestExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+        private bool GuestExists(long id) =>     
+        _context.Guest.Any(e => e.id == id);
 
     }
 }
